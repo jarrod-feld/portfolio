dde337e15de5e9afaa432bf253d7b4ebd9f8d240
jdevin814@gmail.com <jdevin814@gmail.com>
2025-01-24 00:09:55 -0800

    Change project Icons

--- data/index.ts
+++ data/index.ts
@@ -15,28 +15,11 @@ export const navItems = [
     },
     {
       id: 2,
-      title: "Vader Suit",
-      des: "3D printed Darth Vader Suit with integrated Ventilation system, voice changer, lighting, and sound effects.",
+      title: "Maxxed AI",
+      des: "A GPT Personal Development application aimed at providing personalized advice for improving skin care and fitness",
       img: "/p1.png",
-      iconLists: ["/next.svg","/re.svg", "/tail.svg", "/ts.svg","/mongo.svg"],
-      link: "github.com/AkashJana18/nextjs-project",
-    },
-    {
-      id: 3,
-      title: "Cryptack - Cryptocurrency tracker",
-      des: "Compare different cryptocurrencies, view their graphs—prices, total volumes, market cap, etc.",
-      img: "/p2.png",
-      iconLists: ["/re.svg", "/css.svg", "/js.svg", "/chartjs.svg", "/fm.svg", "/mui.svg"],
-      link: "cryptack.netlify.app/",
-    },
-  
-    {
-      id: 4,
-      title: "React Quiz with Timer",
-      des: "React Quiz with Timer and Score is a React-based quiz app that tracks scores and time in real-time.",
-      img: "/p4.png",
-      iconLists: ["/re.svg",  "/css.svg", "/js.svg"],
-      link: "react-quiz-with-timer.netlify.app/",
+      iconLists: ["/next.svg","/re.svg", "/tail.svg", "/ts.svg"],
+      link: "https://github.com/ashakeem/looksai",
     },
   ];
   
