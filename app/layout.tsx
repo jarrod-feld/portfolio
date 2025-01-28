a8a724494030f9d913331e83fe6f55b8b891d7de
jdevin814@gmail.com <jdevin814@gmail.com>
2025-01-28 12:16:59 -0800

    Fixed Tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./provider";
-
+import { GoogleAnalytics } from '@next/third-parties/google'
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -17,6 +17,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+            
+<GoogleAnalytics gaId="G-6CTSHNZ5WZ" />
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,8 @@ import { navItems } from "@/data";
 export default function Home() {
   return (
     <main className="bg-black-100 relative flex justify-center items-center flex-col overflow-clip mx-auto sm:px-10 px-5 w-full">
+  
+
       <div className="max-w-7xl w-screen bg-black">
         <FloatingNav 
         navItems={navItems} />
@@ -17,15 +19,8 @@ export default function Home() {
         {/* <Client /> */}
         
         <Footer />
-        <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-6CTSHNZ5WZ"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+      
 
-  gtag('config', 'G-6CTSHNZ5WZ');
-</script>
       </div>
     </main>
   );
