608850a59370609cd1b63f86f54333c0c4323270
jdevin814@gmail.com <jdevin814@gmail.com>
2025-01-28 22:36:05 -0800

    Changed link

--- data/index.ts
+++ data/index.ts
@@ -11,7 +11,7 @@ export const navItems = [
       des: "TuneShare enables Spotify Premium users to host a Music Room in which all participants can listen to the same songs together.",
       img: "/p2.png",
       iconLists: ["/re.svg",  "/tail.svg","/js.svg"],
-      link: "tune-room.vercel.app",
+      link: "https://www.youtube.com/watch?v=mjOSOQDjJMs",
     },
     {
       id: 2,
