9df5c29cfebd8e4aefd7364123982b950cab9e9c
Jarrod Feld <55763566+jarrod-feld@users.noreply.github.com>
2025-03-06 18:24:32 -0800

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -10,7 +10,7 @@ export const navItems = [
       title: "FirstYearAI",
       des: "Utilizes fine-tuned Gemini model to give First Year College Students a roadmap to learn skills",
       img: "/p5.png",
-      iconLists: ["/ts.svg"],
+      iconLists: ["/ts.svg",'/tail.svg'],
       link: "/www.firstyearai.app",
     },
         {
